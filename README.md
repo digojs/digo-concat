4ef62d02e2913f9965dcec57aaf5f8cb7ed5891a
xuld <xuld@vip.qq.com>
2015-10-10 14:09:03 +0800

    导入项目

---
+++ README.md
@@ -0,0 +1,30 @@
+﻿# digo-concat
+[digo](https://github.com/digojs/digo) 插件：合并文件。
+
+## 安装
+```bash
+npm install digo-concat -g
+```
+
+## 用法
+### 合并所有文件为一个
+```js
+digo.src("*.*").pipe("digo-concat", "out.txt").dest("output/");
+```
+
+### 源映射(Source Map)
+本插件支持生成源映射，详见 [源映射](https://github.com/digojs/digo/wiki/源映射)。
+
+## 选项
+```js
+digo.src("*.js").pipe("digo-concat", {
+	path: "combo.js",		// 合并后文件的路径。默认为原文件夹名。
+	seperator: "\n",        // 每个文件的分隔符。可以是文本或函数。函数原型为：seperator(file, index)。
+	prependEach: "",        // 每个文件顶部追加的文本。可以是文本或函数。函数原型为：prependEach(file, index)。
+	appendEach: "",         // 每个文件底部追加的文本。可以是文本或函数。函数原型为：appendEach(file, index)。
+	prepend: "",            // 最终文件底部追加的文本。可以是文本或函数。函数原型为：prepend()。
+	append: "",             // 最终文件底部追加的文本。可以是文本或函数。函数原型为：append()。
+	content: null,          // 返回每个文件内容的回调函数。函数原型为：content(file)，函数返回 null 则使用原始文件内容。
+	sort: null,          	// 用于排序的回调函数。函数原型为：sort(file1, file2)，函数返回数字，正负性表示顺序。
+}).dest("output/");
+```
---
+++ index.js
@@ -0,0 +1,64 @@
+﻿var digo = require("digo");
+
+module.exports = {
+
+	load: true,
+
+	end: function (files, options, result) {
+
+		// 格式化参数。
+		if (typeof options === "string") {
+			options = { path: options };
+		} else {
+			options = Object.assign({}, options);
+		}
+		if (options.seperator == undefined) {
+			options.seperator = "\n";
+		}
+
+		// 排序。
+		if (options.sort) {
+			files = files.slice(0);
+			files.sort(options.sort);
+		}
+
+		var resultFile = new digo.File();
+		resultFile.path = options.path || (files[0] ? digo.getFileName(files[0].dir) + files[0].ext : "concat");
+		var writer = resultFile.createWriter();
+
+		if (options.prepend) {
+			writer.write(typeof options.prepend === "function" ? options.prepend() : options.prepend);
+		}
+
+		for (var i = 0; i < files.length; i++) {
+			var file = files[i];
+
+			if (i && options.seperator) {
+				writer.write(typeof options.seperator === "function" ? options.seperator(file, i) : options.seperator);
+			}
+
+			if (options.prependEach) {
+				writer.write(typeof options.prependEach === "function" ? options.prependEach(file, i) : options.prependEach);
+			}
+
+			if (typeof options.content === "function") {
+				writer.write(options.content(file, i));
+			} else {
+				writer.writeFile(file);
+			}
+
+			if (options.appendEach) {
+				writer.write(typeof options.appendEach === "function" ? options.appendEach(file, i) : options.appendEach);
+			}
+
+		}
+
+		if (options.append) {
+			writer.write(typeof options.append === "function" ? options.append() : options.append);
+		}
+
+		writer.end();
+		result.add(resultFile);
+	}
+
+};
